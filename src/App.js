7c84093011006cf62050de13953ae83e2f81b39c
ParagMhrjn <chandra_parag@hotmail.com>
2024-08-14 19:55:52 +0200

    ui change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a react app.
+          This is a react app. and other updates as well
         </p>
         <a
           className="App-link"
