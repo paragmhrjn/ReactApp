965a121552d3f4ad40599410e01d30459d37e4a8
ParagMhrjn <chandra_parag@hotmail.com>
2024-08-16 21:14:38 +0200

    updated features

--- src/components/sidebar/Siderbar.jsx
+++ src/components/sidebar/Siderbar.jsx
@@ -16,7 +16,6 @@ export default function Siderbar() {
     closed:{
       clipPath: "circle(30px at 50px 50px)",
       transition: {
-        delay: 0.5,
         type: "spring",
         stiffness: 400,
         damping: 40,
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Links = () => {
   return (
-    <div>Links</div>
+    <div className='links'>Links</div>
   )
 }
